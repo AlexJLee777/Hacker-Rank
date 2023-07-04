ca686939a38b438a9272a9472b062ff2687d6140
Alex <alexj.lee777@gmail.com>
2023-07-03 22:37:39 -0700

    Lonely Integer

---
+++ python/Lonely_Integer.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    d = {}
+    for n in a:
+        try:
+            d[n] += 1
+        except:
+            d[n] = 1
+    for n, c in d.items():
+        if c == 1:
+            return n
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
